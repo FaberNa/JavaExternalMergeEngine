98b06b42830dbfc5785c12bf04364e6493542264
Fabrizio Catapano <catapan2@ITLAA0B0D5.lan>
2026-02-24 10:13:47 +0100

    adding test file

--- src/main/java/org/github/faberna/file/split/model/Separator.java
+++ src/main/java/org/github/faberna/file/split/model/Separator.java
@@ -3,7 +3,7 @@ package org.github.faberna.file.split.model;
 import java.io.IOException;
 import java.nio.channels.FileChannel;
 
-public sealed interface Separator permits NewlineSeparator, SingleByteSeparator {
+public sealed interface Separator permits NewlineSeparator, SingleByteSeparator, CustomBytesSeparator {
     /** Ritorna offset subito DOPO il separatore trovato da 'from' in poi, oppure -1 se non c’è fino a EOF. */
     long findNextSeparatorEnd(FileChannel ch, long from, long fileSize) throws IOException;
 }
